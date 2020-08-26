db65d95783357103681bc48b69b69fc0c527bc32
AthenaBC <biancaytchan@gmail.com>
2020-08-26 00:32:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+
+#Variables
+inventory={"Sword":0,"Torche":10,"Enchanting Table":3}
+
+print(inventory["Sword"])
+
+#Story FlowChart
+      #Inventory System
+      #How does it know what things you have?
+
+#Functions
+    #Death & Almost Death
+    #Escape
+    #Welcome
+
+#UI
+    #Fix decision question.
+#Variables
+#Hits
+#Inventory, Variables
+#player Hit, Enemy Hit
+#Scale of Hits
+#Randomize hits of enemy.
